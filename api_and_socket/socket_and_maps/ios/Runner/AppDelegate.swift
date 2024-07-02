c529374898aedd855c59a4acf082f17e529ddfbd
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-07-02 23:24:39 +0545

    adding map and the locations

--- api_and_socket/socket_and_maps/ios/Runner/AppDelegate.swift
+++ api_and_socket/socket_and_maps/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    [GMSServices provideAPIKey:@"AIzaSyDrA3zBemyQ7Dr-RNPUP2CzeAkfuu-4tms"];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ api_and_socket/socket_and_maps/lib/google_map_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  State<GoogleMapPage> createState() {
+    return _GoogleMapPageState();
+  }
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  static const currentLocation = LatLng(37.4223, -122.0848);
+  static const destination = LatLng(39.4223, -122.0848);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: currentLocation,
+          zoom: 13,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId('sourceLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: currentLocation,
+          ),
+          const Marker(
+            markerId: MarkerId('destinationLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: destination,
+          ),
+        },
+      ),
+    );
+  }
+}
