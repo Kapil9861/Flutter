02dc8040d559e1620661e0d1d69174ed8dcf52a9
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-07-04 12:57:45 +0545

    resolved the problem with the kotlin version

--- api_and_socket/socket_and_maps/lib/google_map_page.dart
+++ api_and_socket/socket_and_maps/lib/google_map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class GoogleMapPage extends StatefulWidget {
   const GoogleMapPage({super.key});
@@ -12,7 +13,16 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   static const currentLocation = LatLng(37.4223, -122.0848);
-  static const destination = LatLng(39.4223, -122.0848);
+  static const destination = LatLng(37.3861, -122.0839);
+  final locationController = Location();
+  LatLng? updatedPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) async => await fetchLocationUpdate());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,5 +48,35 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
         ),
       ),
     );
+  }
+
+  Future<void> fetchLocationUpdate() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await locationController.requestService();
+    } else {
+      return;
+    }
+    permissionGranted = await locationController.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await locationController.requestPermission();
+      if (permissionGranted == PermissionStatus.denied) {
+        return;
+      }
+    }
+    locationController.onLocationChanged.listen((updatedLocation) {
+      if (updatedLocation.latitude != null &&
+          updatedLocation.longitude != null) {
+        setState(() {
+          updatedPosition =
+              LatLng(updatedLocation.latitude!, updatedLocation.longitude!);
+        });
+        print(updatedLocation);
+        print("the input position ,must be same as above $updatedLocation");
+      }
+    });
   }
 }
