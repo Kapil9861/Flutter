38a18ec1bd2048c70130e2d847a98fe57b4ca405
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-05 09:55:04 +0545

    Added the navigation service and logic for it and also n empty homepage

--- lets_chat/lib/utils.dart
+++ lets_chat/lib/utils.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 import 'package:lets_chat/firebase_options.dart';
 import 'package:lets_chat/services/auth_services.dart';
+import 'package:lets_chat/services/navigation_service.dart';
 
 Future<void> firebaseSetup() async {
   await Firebase.initializeApp(
@@ -13,5 +14,8 @@ Future<void> registerService() async {
   final GetIt getIt = GetIt.instance;
   getIt.registerSingleton<AuthService>(
     AuthService(),
+  );
+  getIt.registerSingleton<NavigationService>(
+    NavigationService(),
   );
 }
