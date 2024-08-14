0070fa71c6d7ad4163ce1226c9d74c7357111048
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-14 23:45:09 +0545

    Added the news repository to extract data from the api

---
+++ veda_news/lib/repositories/news_repository.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:veda_news/models/news_model.dart';
+import 'package:http/http.dart' as http;
+
+class NewsRepository {
+  Future<NewsModel> fetchNewsFromApi() async {
+    String url =
+        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=978e0e0d995f4a23a96280fcf53ffbef";
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final content = jsonDecode(response.body);
+      return NewsModel.fromJson(content);
+    }
+    throw ErrorDescription(
+        "Could not connect to the source!\n Please try again later");
+  }
+}
