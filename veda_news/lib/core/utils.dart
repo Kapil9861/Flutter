ab6b7abec4d1bfda4debc4c0eeba1c4980fe0568
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-14 22:44:19 +0545

    added the logo and icons alongwith the apis that may be used later

---
+++ veda_news/lib/core/utils.dart
@@ -0,0 +1,3 @@
+String apiKey = "978e0e0d995f4a23a96280fcf53ffbef";
+String everythingApi = "https://newsapi.org/docs/endpoints/everything";
+String topHeadlinesApi = "https://newsapi.org/docs/endpoints/top-headlines";
