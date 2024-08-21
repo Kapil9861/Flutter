6651a16945f49cec0c6c399b39c01156aa8c6b2b
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-21 23:02:42 +0545

    Created the dummy layout for signup

--- flutter_clean/android/app/src/main/kotlin/com/example/flutter_clean/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_clean
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- news_application/veda_news/lib/presentation/pages/auth/signup.dart
+++ news_application/veda_news/lib/presentation/pages/auth/signup.dart
@@ -1,58 +1,28 @@
-// ignore_for_file: use_build_context_synchronously
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_first/auth/forgot_password.dart';
-import 'package:firebase_first/auth/home.dart';
-import 'package:firebase_first/auth/service/auth.dart';
-import 'package:firebase_first/auth/signup.dart';
 import 'package:flutter/material.dart';
 
-class LogIn extends StatefulWidget {
-  const LogIn({super.key});
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
-  State<LogIn> createState() => _LogInState();
+  State<SignUp> createState() => _SignUpState();
 }
 
-class _LogInState extends State<LogIn> {
-  String email = "", password = "";
+class _SignUpState extends State<SignUp> {
+  String email = "", password = "", name = "";
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
 
-  TextEditingController passController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final _formkey = GlobalKey<FormState>();
 
-  Future<void> userLogin() async {
-    try {
-      await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const Home()));
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          backgroundColor: Colors.orangeAccent,
-          content: Text(
-            "No user with the detail was found!",
-            style: TextStyle(fontSize: 20),
-          ),
-        ));
-      } else if (e.code == 'invalid-credential') {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          backgroundColor: Colors.orangeAccent,
-          content: Text(
-            "Invalid user credential!",
-            style: TextStyle(fontSize: 20),
-          ),
-        ));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          backgroundColor: Colors.orangeAccent,
-          content: Text(
-            e.code,
-            style: const TextStyle(fontSize: 20),
-          ),
-        ));
-      }
+  Future<void> register() async {
+    if (password != "" &&
+        name != "" &&
+        email != "" &&
+        password.isNotEmpty &&
+        name.isNotEmpty &&
+        email.isNotEmpty) {
+      try {} catch (e) {}
     }
   }
 
@@ -66,16 +36,16 @@ class _LogInState extends State<LogIn> {
             SizedBox(
                 width: MediaQuery.of(context).size.width,
                 child: Image.asset(
-                  "assets/images/car.PNG",
+                  "",
                   fit: BoxFit.cover,
                 )),
             const SizedBox(
-              height: 30.0,
+              height: 15.0,
             ),
             Padding(
               padding: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: Form(
-                key: _formKey,
+                key: _formkey,
                 child: Column(
                   children: [
                     Container(
@@ -87,20 +57,20 @@ class _LogInState extends State<LogIn> {
                       child: TextFormField(
                         validator: (value) {
                           if (value == null || value.isEmpty) {
-                            return 'Please Enter E-mail';
+                            return 'Please Enter Name';
                           }
                           return null;
                         },
-                        controller: emailController,
+                        controller: nameController,
                         decoration: const InputDecoration(
                             border: InputBorder.none,
-                            hintText: "Email",
+                            hintText: "Name",
                             hintStyle: TextStyle(
                                 color: Color(0xFFb2b7bf), fontSize: 18.0)),
                       ),
                     ),
                     const SizedBox(
-                      height: 30.0,
+                      height: 15.0,
                     ),
                     Container(
                       padding: const EdgeInsets.symmetric(
@@ -109,13 +79,37 @@ class _LogInState extends State<LogIn> {
                           color: const Color(0xFFedf0f8),
                           borderRadius: BorderRadius.circular(30)),
                       child: TextFormField(
-                        controller: passController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Email';
+                          }
+                          return null;
+                        },
+                        controller: mailController,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email",
+                            hintStyle: TextStyle(
+                                color: Color(0xFFb2b7bf), fontSize: 18.0)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 2.0, horizontal: 30.0),
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFedf0f8),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: TextFormField(
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please Enter Password';
                           }
                           return null;
                         },
+                        controller: passwordController,
                         decoration: const InputDecoration(
                             border: InputBorder.none,
                             hintText: "Password",
@@ -125,28 +119,29 @@ class _LogInState extends State<LogIn> {
                       ),
                     ),
                     const SizedBox(
-                      height: 30.0,
+                      height: 15.0,
                     ),
                     GestureDetector(
                       onTap: () {
-                        setState(() {
-                          if (_formKey.currentState!.validate()) {
-                            email = emailController.text;
-                            password = passController.text;
-                          }
-                        });
-                        userLogin();
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            email = mailController.text;
+                            name = nameController.text;
+                            password = passwordController.text;
+                          });
+                          register();
+                        }
                       },
                       child: Container(
                           width: MediaQuery.of(context).size.width,
                           padding: const EdgeInsets.symmetric(
-                              vertical: 13.0, horizontal: 30.0),
+                              vertical: 8.0, horizontal: 30.0),
                           decoration: BoxDecoration(
                               color: const Color(0xFF273671),
                               borderRadius: BorderRadius.circular(30)),
                           child: const Center(
                               child: Text(
-                            "Sign In",
+                            "Sign Up",
                             style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 22.0,
@@ -158,23 +153,7 @@ class _LogInState extends State<LogIn> {
               ),
             ),
             const SizedBox(
-              height: 20.0,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ForgotPassword()));
-              },
-              child: const Text("Forgot Password?",
-                  style: TextStyle(
-                      color: Color(0xFF8c8e98),
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w500)),
-            ),
-            const SizedBox(
-              height: 40.0,
+              height: 25.0,
             ),
             const Text(
               "or LogIn with",
@@ -184,15 +163,13 @@ class _LogInState extends State<LogIn> {
                   fontWeight: FontWeight.w500),
             ),
             const SizedBox(
-              height: 30.0,
+              height: 15.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GestureDetector(
-                  onTap: () {
-                    AuthMethods().signinWithGoogle(context);
-                  },
+                  onTap: () {},
                   child: Image.asset(
                     "assets/images/google.png",
                     height: 45,
@@ -201,12 +178,10 @@ class _LogInState extends State<LogIn> {
                   ),
                 ),
                 const SizedBox(
-                  width: 30.0,
+                  width: 20.0,
                 ),
                 GestureDetector(
-                  onTap: () {
-                    AuthMethods().signInWithApple(context);
-                  },
+                  onTap: () {},
                   child: Image.asset(
                     "assets/images/apple1.png",
                     height: 50,
@@ -217,12 +192,12 @@ class _LogInState extends State<LogIn> {
               ],
             ),
             const SizedBox(
-              height: 40.0,
+              height: 30.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Don't have an account?",
+                const Text("Already have an account?",
                     style: TextStyle(
                         color: Color(0xFF8c8e98),
                         fontSize: 18.0,
@@ -232,16 +207,14 @@ class _LogInState extends State<LogIn> {
                 ),
                 GestureDetector(
                   onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUp()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Scaffold()));
                   },
                   child: const Text(
-                    "SignUp",
+                    "LogIn",
                     style: TextStyle(
                         color: Color(0xFF273671),
-                        fontSize: 20.0,
+                        fontSize: 18.0,
                         fontWeight: FontWeight.w500),
                   ),
                 ),
