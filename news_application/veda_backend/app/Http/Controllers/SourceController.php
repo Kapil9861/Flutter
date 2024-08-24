87657b5862c73399058f5a4afe7e54bc64853883
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-24 20:02:49 +0545

    Added the function to add the source in the database

--- news_application/veda_backend/app/Http/Controllers/SourceController.php
+++ news_application/veda_backend/app/Http/Controllers/SourceController.php
@@ -11,4 +11,21 @@ class SourceController extends Controller
         $companies=Source::all();
         return $companies;
     }
+
+    public function store(Request $request){
+        $validatedData= $request->validate(["name"=>"required|string|max:30|unique:sources,name"]);
+        $existingSource = Source::where('name', $validatedData['name'])->first();
+        if ($existingSource) {
+            // If the source already exists, return an error message or redirect back
+             return response()->json([
+                'error' => 'The source name already exists!',
+            ], 409);
+        }
+        $source=new Source;
+        $source->name=$validatedData['name'];
+        $source->save();
+        return response()->json([
+            'success' => 'Source created successfully.',
+        ], 200);
+    }
 }
