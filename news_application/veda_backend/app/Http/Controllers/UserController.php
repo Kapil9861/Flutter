af1b54b249c108be0460e171de7455c1b1802d67
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-08-26 23:51:12 +0545

    created a function to create a new user

--- news_application/veda_backend/app/Http/Controllers/UserController.php
+++ news_application/veda_backend/app/Http/Controllers/UserController.php
@@ -2,14 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function store(Request $request){
         $validator=Validator::make($request->all,[
-            'source_id'=>"required"
+            'source_id'=>"required|exists:sources,id",
+            'name'=>'required|string|max:45',
+            'email'=>'required|string|email|max:255|unique:users,email',
+            "password"=>"required|string|min:8",
         ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => true, 'message' => $validator->errors()->first()], 400);
+        }
+        $user=new User();
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->source_id = $request->source_id; // Save the source ID if provided
+        $user->save();
+
+        return response()->json([
+            'success' => 'User created successfully.',
+            'user' => $user,
+        ], 201);
     }
+    
 }
