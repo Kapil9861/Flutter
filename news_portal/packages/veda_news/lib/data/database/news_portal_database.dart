c1ba946448376506fa2921866d7c5cdd75a8aee1
unknown <Kapil Aryal>
2024-10-03 16:58:14 +0545

    created the functions for adding, removing and getting the facvourites and followed sources from the database

--- news_portal/packages/veda_news/lib/data/database/news_portal_database.dart
+++ news_portal/packages/veda_news/lib/data/database/news_portal_database.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
@@ -21,6 +22,56 @@ class NewsPortalDatabase extends _$NewsPortalDatabase {
 
   @override
   int get schemaVersion => 1;
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      beforeOpen: (details) async {
+        if (kDebugMode) {
+          print(details);
+        }
+      },
+      onCreate: (m) async {
+        await m.createAll();
+      },
+      onUpgrade: (m, from, to) async {
+        if (from < 2) {
+          await m.createTable(favourites);
+          await m.createTable(followedSource);
+        }
+      },
+    );
+  }
+
+  Future<int> addFavouriteArticle(FavouritesCompanion favourite) async {
+    final query = await (into(favourites).insert(favourite));
+    return query;
+  }
+
+  Future<int> addFollowedSource(FollowedSourceCompanion source) async {
+    return await (into(followedSource).insert(source));
+  }
+
+  Future<int> removeFollowedSource(int sourceId) async {
+    final query = await (delete(favourites)
+          ..where((table) => table.id.equals(sourceId)))
+        .go();
+    return query;
+  }
+
+  Future<int> removeFavouriteArticle(int id) async {
+    final query = await (delete(followedSource)
+          ..where((table) => table.id.equals(id)))
+        .go();
+    return query;
+  }
+
+  Stream<List<FollowedSourceData>> getFollowedSources() {
+    return (select(followedSource).watch());
+  }
+
+  Stream<List<Favourite>> getFavouriteArticle() {
+    return (select(favourites).watch());
+  }
 }
 
 // Function to open the database connection
