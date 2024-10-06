6ced35024545aa478966cbd947ffa9cd14478cf9
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-10-06 23:47:36 +0545

    Added the favourite channel tile

--- news_portal/packages/veda_news/lib/presentation/pages/favourites_news_channel_article.dart
+++ news_portal/packages/veda_news/lib/presentation/pages/favourites_news_channel_article.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:veda_news/presentation/providers/drift/followed_source_provider.dart';
+import 'package:veda_news/presentation/providers/logo_provider.dart';
+import 'package:veda_news/presentation/widgets/styled_text.dart';
 
 class FavouritesNewsChannelArticle extends ConsumerStatefulWidget {
   const FavouritesNewsChannelArticle({
@@ -18,20 +19,36 @@ class FavouritesNewsChannelArticle extends ConsumerStatefulWidget {
 
 class _FavouritesNewsChannelArticleState
     extends ConsumerState<FavouritesNewsChannelArticle> {
+  final _logoProvider = LogoProvider();
   @override
   Widget build(BuildContext context) {
+    final logo = _logoProvider.getLogo(widget.sourceName);
     return Padding(
       padding: const EdgeInsets.all(8),
       child: GestureDetector(
         onTap: () {},
         child: Container(
+            height: 60,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [],
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Image.network(logo),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: StyledText(fontSize: 14, text: widget.sourceName),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Follow"),
+                ),
+              ],
             )),
       ),
     );
