20d7a016a86f26cec94a925745360a9b1cb044d9
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-10-20 23:31:32 +0545

    added the login and register pages

--- news_portal/packages/veda_news/lib/presentation/pages/login/register.dart
+++ news_portal/packages/veda_news/lib/presentation/pages/login/register.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:veda_news/data/models/source.dart';
+import 'package:veda_news/presentation/pages/login/login.dart';
+import 'package:veda_news/presentation/widgets/styled_text.dart';
 
 class Register extends ConsumerStatefulWidget {
   const Register({super.key});
@@ -9,8 +12,231 @@ class Register extends ConsumerStatefulWidget {
 }
 
 class _RegisterState extends ConsumerState<Register> {
+  String email = "", password = "", name = "", phoneNumber = "";
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  DropdownMenuItem<String> notFound = const DropdownMenuItem<String>(
+    value: "not-found",
+    child: Text("Not found"),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final _formkey = GlobalKey<FormState>();
+  String? selectedValue;
+  String? deviceName;
+
+  TextEditingController companyName = TextEditingController();
+  bool _isVisible = true;
+  FocusNode sourceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SizedBox(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 170,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    "assets/images/news1.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                const StyledText(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  text: "Personal Information",
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Form(
+                    key: _formkey,
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2.0, horizontal: 30.0),
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFedf0f8),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Name';
+                              }
+                              return null;
+                            },
+                            controller: nameController,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Name",
+                              hintStyle: TextStyle(
+                                color: Color(0xFFb2b7bf),
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2.0, horizontal: 30.0),
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFedf0f8),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Phone Number';
+                              }
+                              return null;
+                            },
+                            controller: phoneController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "+977 9876543210",
+                                hintStyle: TextStyle(
+                                    color: Color(0xFFb2b7bf), fontSize: 18.0)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2.0, horizontal: 30.0),
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFedf0f8),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Phone Number';
+                              }
+                              return null;
+                            },
+                            controller: mailController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email",
+                                hintStyle: TextStyle(
+                                    color: Color(0xFFb2b7bf), fontSize: 18.0)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 2.0, horizontal: 30.0),
+                          decoration: BoxDecoration(
+                              color: const Color(0xFFedf0f8),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter Password';
+                              }
+                              return null;
+                            },
+                            controller: passwordController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(
+                                    color: Color(0xFFb2b7bf), fontSize: 18.0)),
+                            obscureText: true,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            if (_formkey.currentState!.validate()) {
+                              setState(() {
+                                email = mailController.text;
+                                name = nameController.text;
+                                password = passwordController.text;
+                                phoneNumber = phoneController.text;
+                              });
+                            }
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 30.0),
+                            decoration: BoxDecoration(
+                                color: const Color(0xFF273671),
+                                borderRadius: BorderRadius.circular(30)),
+                            child: const Center(
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Already have an account?",
+                        style: TextStyle(
+                            color: Color(0xFF8c8e98),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500)),
+                    const SizedBox(
+                      width: 5.0,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "LogIn",
+                        style: TextStyle(
+                            color: Color(0xFF273671),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
