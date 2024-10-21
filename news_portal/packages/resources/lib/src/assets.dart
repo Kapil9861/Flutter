1058bdf7459a933debc33b3508d731e1194a31de
unknown <Kapil Aryal>
2024-10-21 11:34:29 +0545

    managed the assets

--- news_portal/packages/resources/lib/src/assets.dart
+++ news_portal/packages/resources/lib/src/assets.dart
@@ -1,6 +1,8 @@
 class Assets {
-  static const String frame = 'assets/icons/Frame.png';
-  static const String more = 'assets/icons/more.png';
-  static const String share = 'assets/icons/share.png';
-  static const String vector = 'assets/icons/vector.png';
+  static const String frame = 'packages/resources/lib/assets/icons/Frame.png';
+  static const String more = 'packages/resources/lib/assets/icons/more.png';
+  static const String share = 'packages/resources/lib/assets/icons/share.png';
+  static const String vector = 'packages/resources/lib/assets/icons/vector.png';
+  static const String registerBg =
+      'packages/resources/lib/assets/images/news1.jpg';
 }
